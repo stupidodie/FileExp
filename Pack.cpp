391a698bc8f830b3fcf3a260121d318142576860
Patrickqyp <73684731+Patrickqyp@users.noreply.github.com>
2020-12-19 21:45:36 +0800

    文件打包解包的功能实现
    Pack.cpp实现的是文件的目录树打包和解包功能，打包是将若干目录和文件按照目录树的结构进行遍历，并按照这个顺序打包成一个文件；解包是打包的逆过程，将一个打包文件恢复成多个文件，并恢复文件的目录树结构。

---
+++ Pack.cpp
@@ -0,0 +1,228 @@
+//
+// Created by tgr on 2020/11/10.
+//
+
+#include "Pack.h"
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+std::string getFileNameFromPath(const std::string &path) {
+    fs::path p(path);
+    return p.filename().string();
+}
+
+// subFiles 包含目录、文件的信息结构体
+// 顺序呢
+void GetAllSubFiles(const fs::path &directoryPath, std::vector<SubFileInfo> &subFiles) {
+    subFiles.emplace_back(false, fs::absolute(directoryPath).string());
+    for (auto &entry: fs::directory_iterator(directoryPath)) {
+        if (entry.is_directory()) {
+            GetAllSubFiles(entry.path(), subFiles);
+        } else {
+            subFiles.emplace_back(true, entry.file_size(), fs::absolute(entry.path()).string());
+        }
+    }
+}
+
+bool packFileAndFolder(const std::vector<std::string> &Files,
+                       const std::vector<std::string> &Folders,
+                       const std::string &OutputFile) {
+    //打包格式:多少个文件 然后每个文件的名称存储，然后是文件的内容
+    FILE *output = fopen(OutputFile.c_str(), "wb+");
+    if (output == nullptr) {
+        std::cout << "Open OutputFile Error!" << std::endl;
+        return false;
+    } else {
+        //文件打包部分
+        int fileNumber = Files.size();//统计文件个数
+        fwrite(&fileNumber, sizeof(fileNumber), 1, output);//将有多少个文件个数写入
+        printf("pack files:\n");
+        for (auto &file : Files) {
+            auto fileSize = fs::file_size(file);
+            FileInfo info(getFileNameFromPath(file), fileSize);
+            //FIleInfo写如文件
+            info.writeToFile(output);// NOLINT(bugprone-sizeof-expression)
+            //把文件具体内容写如
+            if (fileSize != 0) {
+                FILE *input = fopen(file.c_str(), "rb+");//打开输入文件
+                if (input != nullptr) {
+                    auto *fileBuffer = new unsigned char[fileSize];
+                    fread(fileBuffer, fileSize, 1, input);
+                    fwrite(fileBuffer, fileSize, 1, output);
+                    delete[] fileBuffer;
+                    fclose(input);
+                }
+            }
+        }
+        //文件夹打包部分
+        printf("pack folders\n");
+
+
+        int totalFolderFileNum = 0;
+        for (auto &Folder : Folders) { //Folders是绝对路径
+            std::vector<SubFileInfo> SubFiles;
+            GetAllSubFiles(Folder, SubFiles);
+            totalFolderFileNum += SubFiles.size(); //所有文件总个数
+        }
+
+        fwrite(&totalFolderFileNum, sizeof(totalFolderFileNum), 1, output);//将有多少个文件和文件夹写入
+        for (auto &folder : Folders) {
+            std::vector<SubFileInfo> AllSubFiles;
+            GetAllSubFiles(folder, AllSubFiles);
+
+            //
+            //获取文件名字
+            //  C:/users/xhy/  b/c/a.txt
+            std::string subDirectory = getFileNameFromPath(folder);//输入C:/a/c/b  输出 /b
+            std::cout << subDirectory << std::endl;
+            unsigned long pos = folder.length() - subDirectory.length();//定位前面字符串最后的位置
+            for (auto SubFile : AllSubFiles) {
+                std::string absolutePath = SubFile.filename;
+                SubFile.filename = SubFile.filename.erase(0, pos); //删除绝对路径部分 C:/a/c
+                //std::cout<<AllSubFile.filename<<std::endl;
+                if (SubFile.IsTypeFile) {
+                    // std::cout << absolutePath << std::endl;
+                    unsigned long long int fileSize = fs::file_size(absolutePath);//获取文件大小
+                    SubFile.fileSize = fileSize;
+                    SubFile.writeToFile(output);//fwrite(&SubFile, sizeof(SubFileInfo), 1, output);
+                    assert (fileSize != 0);
+                    FILE *input = fopen(absolutePath.c_str(), "rb+");//打开输入文件
+                    assert(input != nullptr);
+                    std::unique_ptr<unsigned char> fileBuffer(new unsigned char[fileSize]);
+                    fread(fileBuffer.get(), fileSize, 1, input);
+                    fwrite(fileBuffer.get(), fileSize, 1, output);
+                    fclose(input);
+                } else {
+                    SubFile.writeToFile(output);//fwrite(&SubFile, sizeof(SubFileInfo), 1, output);
+                }
+            }
+        }
+    }
+    fclose(output);
+    return true;
+}
+
+bool unPackFileAndFolder(const std::string &inputFilePath, const std::string &OutputPath) {
+    FILE *input = fopen(inputFilePath.c_str(), "rb+");
+    fs::create_directory(OutputPath);
+    if (input == nullptr) {
+        std::cout << "Input file Path Error! Unpack Failure!" << std::endl;
+        return false;
+    }
+    //文件解包部分
+    int fileNumber;
+    fread(&fileNumber, sizeof(fileNumber), 1, input);
+    for (auto i = 0; i < fileNumber; i++) {
+        struct FileInfo fileInformation;//用于保存文件信息
+        fileInformation.readFromFile(
+                input);//fread(&fileInformation, sizeof(fileInfo), 1, input); // NOLINT(bugprone-sizeof-expression)
+        std::string fileOutputPath = OutputPath + "/" + fileInformation.fileName;
+        FILE *output = fopen(fileOutputPath.c_str(), "wb+");
+        if (output == nullptr) {
+            std::cout << "Open file Error!" << std::endl;
+            return false;
+        }
+        auto *fileBuffer = new unsigned char[fileInformation.fileSize];
+        fread(fileBuffer, fileInformation.fileSize, 1, input);
+        fwrite(fileBuffer, fileInformation.fileSize, 1, output);
+        delete[] fileBuffer;
+        fclose(output);
+    }
+    //文件夹解包部分
+    int totalFolderFileNum;
+    fread(&totalFolderFileNum, sizeof(totalFolderFileNum), 1, input);//将有多少个文件和文件夹写入
+    for (auto i = 0; i < totalFolderFileNum; i++) {
+        struct SubFileInfo subFileInfo;
+        subFileInfo.readFromFile(input);//fread(&subFileInfo, sizeof(SubFileInfo), 1, input);
+        std::string path = OutputPath + "/" + subFileInfo.filename;
+        std::cout << "Unpack " << subFileInfo.filename << " to " << path << std::endl;
+        std::cout << subFileInfo.IsTypeFile << std::endl;
+        if (!subFileInfo.IsTypeFile) {//如果是文件夹
+            fs::create_directory(fs::path(path));
+        } else {
+            FILE *output = fopen(path.c_str(), "wb+");
+            if (!output) {
+                std::cout << "Open file(" << path << ") Error!" << std::endl;
+                return false;
+            }
+            std::unique_ptr<unsigned char> fileBuffer(new unsigned char[subFileInfo.fileSize]);
+            fread(fileBuffer.get(), subFileInfo.fileSize, 1, input);
+            fwrite(fileBuffer.get(), subFileInfo.fileSize, 1, output);
+            fclose(output);
+        }
+    }
+
+    fclose(input);
+
+    return true;
+}
+
+//#endif
+
+
+void FileInfo::writeToFile(FILE *file) {
+    assert(file != nullptr);
+    auto serialization(serialize());
+    std::unique_ptr<char> data(serialization.first);
+    auto *len_ptr(&serialization.second);
+    fwrite(len_ptr, sizeof(*len_ptr), 1, file);
+    fwrite(data.get(), *len_ptr, 1, file);
+
+}
+
+void FileInfo::readFromFile(FILE *file) {
+    assert(file != nullptr);
+    std::unique_ptr<int> len_ptr(new int);
+    fread(len_ptr.get(), sizeof(*len_ptr), 1, file);
+    std::unique_ptr<char> data(new char[*len_ptr]);
+    fread(data.get(), *len_ptr, 1, file);
+    deserialize(data.get(), *len_ptr);
+}
+
+std::pair<char *, int> FileInfo::serialize() {
+    size_t len(fileName.size() + sizeof(fileSize));
+    char *data(new char[len]);
+    memcpy(data, &fileSize, sizeof(fileSize));
+    memcpy(data + sizeof(fileSize), fileName.c_str(), fileName.size());
+    return std::pair<char *, int>(data, len);
+}
+
+void FileInfo::deserialize(char *data, int len) {
+    memcpy(&fileSize, data, sizeof(fileSize));
+    fileName = std::string(data + sizeof(fileSize), len - sizeof(fileSize));
+}
+
+void SubFileInfo::writeToFile(FILE *file) {
+    assert(file != nullptr);
+    auto serialization(serialize());
+    std::unique_ptr<char> data(serialization.first);
+    auto *len_ptr(&serialization.second);
+    fwrite(len_ptr, sizeof(*len_ptr), 1, file);
+    fwrite(data.get(), *len_ptr, 1, file);
+}
+
+void SubFileInfo::readFromFile(FILE *file) {
+    assert(file != nullptr);
+    std::unique_ptr<int> len_ptr(new int);
+    fread(len_ptr.get(), sizeof(*len_ptr), 1, file);
+    std::unique_ptr<char> data(new char[*len_ptr]);
+    fread(data.get(), *len_ptr, 1, file);
+    deserialize(data.get(), *len_ptr);
+}
+
+std::pair<char *, int> SubFileInfo::serialize() {
+    size_t len(filename.size() + sizeof(IsTypeFile) + sizeof(fileSize));
+    char *data(new char[len]);
+    memcpy(data, &IsTypeFile, sizeof(IsTypeFile));
+    memcpy(data + sizeof(IsTypeFile), &fileSize, sizeof(fileSize));
+    memcpy(data + sizeof(IsTypeFile) + sizeof(fileSize), filename.c_str(), filename.size());
+    return std::pair<char *, int>(data, len);
+}
+
+void SubFileInfo::deserialize(char *data, int len) {
+    memcpy(&IsTypeFile, data, sizeof(IsTypeFile));
+    memcpy(&fileSize, data + sizeof(IsTypeFile), sizeof(fileSize));
+    filename = std::string(data + sizeof(IsTypeFile) + sizeof(fileSize),
+                           len - sizeof(IsTypeFile) - sizeof(fileSize));
+}
---
+++ Pack.h
@@ -0,0 +1,76 @@
+//
+// Created by tgr on 2020/11/10.
+//
+
+#ifndef FILEPACK_PACK_H
+#define FILEPACK_PACK_H
+
+#include <string>
+#include <utility>
+#include <vector>
+#include <cstring>
+#include <cassert>
+#include <memory>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+struct FileInfo {
+    std::string fileName;  //绝对路径
+    unsigned long long int fileSize{}; //B
+    FileInfo() = default;
+
+    FileInfo(std::string fileName, unsigned long long int size) : fileName(std::move(fileName)), fileSize(size) {}
+
+    // 确保file指针有效
+    void writeToFile(FILE *file);
+
+    void readFromFile(FILE *file);
+
+private:
+    // 不考虑大小端
+    // 记得释放内存
+    std::pair<char *, int> serialize();
+
+    void deserialize(char *data, int len);
+};
+
+struct SubFileInfo {
+    ~SubFileInfo() = default;
+
+    SubFileInfo(bool isFile, unsigned long long int size, std::string name)
+            : IsTypeFile(isFile), fileSize(size), filename(std::move(name)) {}
+
+    SubFileInfo() = default;
+
+    SubFileInfo(bool isFile, std::string name) : SubFileInfo(isFile, 0, std::move(name)) {}
+
+    bool IsTypeFile;//纪录类型是文件夹还是文件 true是文件 false是文件夹
+    unsigned long long int fileSize;//如果是文件的话，则存储大小
+    std::string filename;
+
+    // 确保file指针有效
+    void writeToFile(FILE *file);
+
+    void readFromFile(FILE *file);
+
+private:
+    // 不考虑大小端
+    // 记得释放内存
+    std::pair<char *, int> serialize();
+
+    void deserialize(char *data, int len);
+};
+
+//给定一个文件夹的路径，获取其下面所有子文件
+void GetAllSubFiles(const fs::path &directoryPath, std::vector<SubFileInfo> &subFiles);
+
+//打包文件和文件夹
+bool packFileAndFolder(const std::vector<std::string> &Files, const std::vector<std::string> &Folders,
+                       const std::string &OutputFile);
+
+bool unPackFileAndFolder(const std::string &inputFilePath, const std::string &OutputPath);
+
+std::string getFileNameFromPath(const std::string &path);
+
+#endif //FILEPACK_PACK_H
